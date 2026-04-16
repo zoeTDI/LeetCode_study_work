fd6c3563ea5abd16b53b3fd558322b60c08eac53
caldm <1832400547@qq.com>
2026-04-16 10:49:37 +0800

    修改lengthOfLongestSubstring方法的内部实现 - 使用数组存放每个字符最后出现的位置，速度更快。

--- src/main/java/org/example/section1_10/Solution.java
+++ src/main/java/org/example/section1_10/Solution.java
@@ -142,18 +142,13 @@ public class Solution {
     }
 
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() < 2) return s.length();
-        HashMap<Character, Integer> map = new HashMap<>(127);
-        map.put(s.charAt(0), 0);
+        int[] nums = new int[128];
         int left = 0;
-        int max = 1 - left;
-        for (int i = 1; i < s.length(); i++) {
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if (map.containsKey(ch)) {
-                left = Math.max(left, map.get(ch) + 1);
-            }
-            // update the character last of index
-            map.put(ch, i);
+            left = Math.max(left, nums[ch]);
+            nums[ch] = i + 1;
             max = Math.max(max, i - left + 1);
         }
         return max;
